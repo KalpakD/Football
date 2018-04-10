42b0f6df890a72144b5e5bbe2fecc9f58ea038fe
kalpakdeshpande <kalpak.deshpande@gmail.com>
2018-04-10 10:09:18 +0530

    updated

--- app/src/main/java/com/example/kalpak/football/utils/DateTime.java
+++ app/src/main/java/com/example/kalpak/football/utils/DateTime.java
@@ -1,147 +1,67 @@
 package com.example.kalpak.football.utils;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public class DateTime {
-    public static String getCurrentData ()
-    {
+    public static String getCurrentData() {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
         Calendar cal = Calendar.getInstance();
         return dateFormat.format(cal.getTime());
 
     }
 
-    public static Calendar getCurrentDataCalendar()
-    {
-        return  Calendar.getInstance();
-    }
 
-
-    public static String getCurrentDataTime ()
-    {
+    public static String getCurrentDataTime() {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return dateFormat.format(new Date());
     }
 
-    public static String getYesterdayData ()
-    {
+    public static String getYesterdayData() {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
         Calendar cal = Calendar.getInstance();
         cal.add(Calendar.DAY_OF_YEAR, -1);
         return dateFormat.format(cal.getTime());
     }
 
-    public static String [] getFirstDayNextYearAndLastDayBeforeYear ()
-    {
-        String [] datas = new String [2];
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.YEAR, 1);
-        cal.set(Calendar.DAY_OF_MONTH, 1);
-        cal.set(Calendar.MONTH, 0);
-        datas [0] = dateFormat.format(cal.getTime());
-        cal = Calendar.getInstance();
-        cal.add(Calendar.YEAR, -1);
-        cal.set(Calendar.DAY_OF_MONTH, 31);
-        cal.set(Calendar.MONTH, 11);
-        datas [1] = dateFormat.format(cal.getTime());
-        return datas;
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public  String convertTime(String time) {
+
+        String converted_date = "";
+        try {
+
+            DateFormat utcFormat = new SimpleDateFormat("HH:mm:ss");
+            utcFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            Date date = utcFormat.parse(time);
+
+            DateFormat currentTFormat = new SimpleDateFormat("HH:mm:ss");
+            currentTFormat.setTimeZone(TimeZone.getTimeZone(getCurrentTimeZone()));
+
+            converted_date = currentTFormat.format(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return converted_date;
     }
 
-    public static String getCurrentDataWithAddSetMonthValue (String first_data, int add_month)
-    {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
-        Calendar cal = Calendar.getInstance();
-        if (!first_data.equals("")) //add first_data in calendar object to correct asign in second data
-        {
-            int urtea = Integer.parseInt(first_data.substring(0, 4));
-            int hilabetea = Integer.parseInt(first_data.substring(5,7))-1;
-            int eguna = Integer.parseInt(first_data.substring(8,10));
-
-            cal.set(urtea, hilabetea, eguna);
-        }
-
-        cal.add(Calendar.MONTH, add_month);  //Add two months to first data
-        return dateFormat.format(cal.getTime());
-
+    public String getCurrentTimeZone() {
+        TimeZone tz = Calendar.getInstance().getTimeZone();
+        System.out.println(tz.getDisplayName());
+        return tz.getID();
     }
 
-    public static String getCurrentDataWithAddSetBeforeDays (String first_data, int add_month)
-    {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
-        Calendar cal = Calendar.getInstance();
-        if (!first_data.equals("")) //add first_data in calendar object to correct asign in second data
-        {
-            int urtea = Integer.parseInt(first_data.substring(0, 4));
-            int hilabetea = Integer.parseInt(first_data.substring(5,7))-1;
-            int eguna = Integer.parseInt(first_data.substring(8,10));
-
-            cal.set(urtea, hilabetea, eguna);
-        }
-
-        cal.add(Calendar.DAY_OF_WEEK, -(add_month));  //DownDays value to first data
-        return dateFormat.format(cal.getTime());
-    }
-
-    public static Calendar getAllDataTimeCalendarObject (String date, String hour_string)
-    {
-        //Extract details data
-        int year = Integer.parseInt(date.substring(0, 4));
-        int month = Integer.parseInt(date.substring(5,7))-1;
-        int day = Integer.parseInt(date.substring(8,10));
-
-        int [] hour_int = getHourMinutesSecondsInfo(hour_string);
-
-        System.out.println(year+"/"+month+"/"+day+" "+hour_int[0]+":"+hour_int[1]+":"+hour_int[2]);
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, day, hour_int[0], hour_int[1], hour_int[2]);
-        return calendar;
-    }
-
-    public static int [] getHourMinutesSecondsInfo (String hour_string)
-    {
-        String hour = hour_string.substring(0, 2);
-        int hour_int;
-        if (hour.charAt(0) == '0')
-        {
-            hour_int = Integer.parseInt(hour.substring(1));
-        }
-        else
-        {
-            hour_int = Integer.parseInt(hour);
-        }
-
-        int minute_int = Integer.parseInt(hour_string.substring(3, 5));
-        return new int [] {hour_int,minute_int,0};
-    }
-
-    public static String [] setFormatData (int year, int monthOfYear, int dayOfMonth)
-    {
-        //year/month/day
-        String [] format_data = new String [3];
-
-        format_data[0] = String.valueOf(year);
-        if (monthOfYear+1 < 10)
-        {
-            format_data [1] = "0"+String.valueOf(monthOfYear+1);
-        }
-        else
-        {
-            format_data [1] = String.valueOf(monthOfYear+1);
-        }
-
-        if (dayOfMonth<10)
-        {
-            format_data [2] = "0"+String.valueOf(dayOfMonth);
-        }
-        else
-        {
-            format_data [2] = String.valueOf(dayOfMonth);
-        }
-        return format_data;
-    }
 }
